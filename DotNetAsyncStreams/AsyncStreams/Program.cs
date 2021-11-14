3bcc3c5159d66027d7bdf5f9f1832b511ef45483
Tamir Dresher <tamirdr@payoneer.com>
2021-11-14 14:59:07 +0200

    basic Enumerable example

---
+++ DotNetAsyncStreams/AsyncStreams/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AsyncStreams
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var transactions = ReadLines("transactions.csv");
+
+            var grps = from t in transactions
+                       where t.usdAmount > 0
+                       group t by t.SrcAccount into g
+                       select g;
+
+            foreach (var grp in grps)
+            {
+                Console.WriteLine($"Key: {grp.Key} Count: {grp.Count()}");
+            }
+        }
+
+        public static IEnumerable<Transaction> ReadLines(string path)
+        {
+            var lines = System.IO.File.ReadAllLines(path);
+            foreach (var line in lines)
+            {
+                string[] arr = line.Split(',');
+                yield return new Transaction(
+                    SrcAccount: arr[0],
+                    DstAccount: arr[1],
+                    timestamp:  arr[2],
+                    usdAmount:  decimal.Parse(arr[3]));
+            }
+        }
+        
+        public record Transaction(string SrcAccount, string DstAccount, string timestamp, decimal usdAmount );
+    }
+}
